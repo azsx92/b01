cf94a40a57806b57fa73223aed8135e9af26fb53
azsx92 <azsx92@gmail.com>
2023-07-24 17:31:06 +0900

    p448 end

---
+++ src/main/java/org/zerock/b01/domain/BaseEntity.java
@@ -0,0 +1,28 @@
+package org.zerock.b01.domain;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import javax.sound.sampled.AudioFileFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(value = {AuditingEntityListener.class})
+@Getter
+public class BaseEntity {
+
+    @CreatedDate
+    @Column(name = "regdate", updatable = false)
+    private LocalDate regDate;
+
+    @LastModifiedDate
+    @Column(name = "moddate")
+    private LocalDateTime modDate;
+}
