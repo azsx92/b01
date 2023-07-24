f678b4c0d834f9c2bb038ef73351b7c21980a284
azsx92 <azsx92@gmail.com>
2023-07-24 16:07:29 +0900

    p421 thymeleaf

--- src/main/java/org/zerock/b01/controller/SampleController.java
+++ src/main/java/org/zerock/b01/controller/SampleController.java
@@ -6,6 +6,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.swing.*;
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 @Log4j2
 public class SampleController {
@@ -16,4 +20,14 @@ public class SampleController {
 
         model.addAttribute("msg", "HELLo WORLD");
     }
+
+    @GetMapping("/ex/ex1")
+    public void ex1(Model model) {
+        log.info("hello...........");
+        List<String> list = Arrays.asList("AAA","BBB","CCC","DDD");
+
+        model.addAttribute("list",list);
+    }
+
+
 }
