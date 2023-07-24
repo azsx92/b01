b485669fae793baeed16c13b768d89d87152f620
azsx92 <azsx92@gmail.com>
2023-07-24 16:30:58 +0900

    p431 5.2 Thymeleaf end

--- src/main/java/org/zerock/b01/controller/SampleController.java
+++ src/main/java/org/zerock/b01/controller/SampleController.java
@@ -6,9 +6,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import javax.swing.*;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 
 @Controller
 @Log4j2
@@ -29,5 +33,46 @@ public class SampleController {
         model.addAttribute("list",list);
     }
 
+    class SampleDTO {
+        private String p1, p2, p3;
 
+        public String getP1() {
+            return p1;
+        }
+        public String getP2() {
+            return p2;
+        }
+        public String getP3() {
+            return p3;
+        }
+    }
+    @GetMapping("/ex/ex2")
+    public void ex2(Model model) {
+        log.info("ex/ex2...........");
+
+
+        List<String> strlist = (List<String>) IntStream.range(1,10).mapToObj(i->"Date"+i)
+                .collect(Collectors.toList());
+
+        model.addAttribute("list",strlist);
+
+        Map<String, String> map = new HashMap<>();
+        map.put("A","AAA");
+        map.put("B","BBB");
+
+        model.addAttribute("map",map);
+
+        SampleDTO sampleDTO = new SampleDTO();
+        sampleDTO.p1 = "Value -- p1";
+        sampleDTO.p2 = "Value -- p2";
+        sampleDTO.p3 = "Value -- p3";
+
+        model.addAttribute("dto",sampleDTO);
+    }
+    @GetMapping("/ex/ex3")
+    public void ex3(Model model) {
+        log.info("ex/ex3...........");
+
+        model.addAttribute("arr",new String[]{"AAA","BBB","CCC"});
+    }
 }
